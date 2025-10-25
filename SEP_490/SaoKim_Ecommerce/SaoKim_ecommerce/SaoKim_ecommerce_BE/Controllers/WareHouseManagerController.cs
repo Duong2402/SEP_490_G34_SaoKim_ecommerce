a7d5a67b742223da316825120bf5980ff6e25d9b
Duong2402 <trduong0224@gmail.com>
2025-10-25 20:07:53 +0700

    connect with postgreSQL

---
+++ SEP_490/SaoKim_Ecommerce/SaoKim_ecommerce/SaoKim_ecommerce_BE/Controllers/WareHouseManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SaoKim_ecommerce_BE.Controllers
+{
+    public class WareHouseManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- SEP_490/SaoKim_Ecommerce/SaoKim_ecommerce/SaoKim_ecommerce_BE/Program.cs
+++ SEP_490/SaoKim_Ecommerce/SaoKim_ecommerce/SaoKim_ecommerce_BE/Program.cs
@@ -1,20 +1,26 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+//using SaoKim_ecommerce_BE.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//builder.Services.AddDbContext<AppDbContext>(options =>
+//    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+//);
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("ViteDev", policy =>
+    options.AddPolicy("AllowFE", policy =>
     {
         policy.WithOrigins("http://localhost:5173")
               .AllowAnyHeader()
-              .AllowAnyMethod()
-              .AllowCredentials();
+              .AllowAnyMethod();
     });
 });
 
@@ -22,12 +28,12 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseCors("ViteDev");
-app.UseAuthorization();
+app.UseCors("AllowFE");
 app.MapControllers();
 
 app.Run();
