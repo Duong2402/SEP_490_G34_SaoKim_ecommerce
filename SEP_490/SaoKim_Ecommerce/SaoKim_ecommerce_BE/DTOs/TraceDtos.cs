e605fbb943274eb86c8ecda8e0cdf7277db056b3
Duong2402 <trduong0224@gmail.com>
2025-11-30 19:38:03 +0700

    30/11/2025 refractor

---
+++ SEP_490/SaoKim_Ecommerce/SaoKim_ecommerce_BE/DTOs/TraceDtos.cs
@@ -0,0 +1,50 @@
+﻿namespace SaoKim_ecommerce_BE.DTOs
+{
+    public class TraceSearchQuery
+    {
+        public string? Query { get; set; }
+    }
+    public class TraceEventDto
+    {
+        public DateTime Time { get; set; }
+        public string? Type { get; set; }
+        public string? Ref { get; set; }
+        public string? Actor { get; set; }
+        public string? Note { get; set; }
+    }
+
+    public class TraceSearchResultDto
+    {
+        public int Id { get; set; }
+        public string Serial { get; set; } = string.Empty;
+        public string? Sku { get; set; }
+        public string? ProductName { get; set; }
+        public string? Status { get; set; }
+        public string? Project { get; set; }
+        public string? CurrentLocation { get; set; }
+
+        public List<TraceEventDto> Timeline { get; set; } = new();
+    }
+    public class ProductTraceMovementDto
+    {
+        public string Direction { get; set; } = "";
+        public string SlipType { get; set; } = ""; 
+        public string? RefNo { get; set; }
+        public string? Partner { get; set; }
+        public DateTime Date { get; set; }
+        public int Quantity { get; set; }
+        public string? Uom { get; set; }
+        public string? Note { get; set; }
+        public int SlipId { get; set; }
+    }
+
+    public class ProductTraceDto
+    {
+        public int ProductId { get; set; }
+        public string? ProductCode { get; set; }
+        public string ProductName { get; set; } = "";
+        public string? Unit { get; set; }
+
+        public List<ProductTraceMovementDto> Movements { get; set; } = new();
+    }
+}
---
+++ SEP_490/SaoKim_Ecommerce/SaoKim_ecommerce_BE/Services/IWarehouseReportService.cs
@@ -0,0 +1,20 @@
+﻿using SaoKim_ecommerce_BE.DTOs;
+using SaoKim_ecommerce_BE.Entities;
+
+namespace SaoKim_ecommerce_BE.Services
+{
+    public interface IWarehouseReportService
+    {
+        Task<List<InboundReportDto>> GetInboundReportAsync(InboundReportQuery q);
+        Task<List<OutboundReportDto>> GetOutboundReportAsync(OutboundReportQuery q);
+        Task<WeeklyInboundSummaryDto> GetWeeklyInboundSummaryAsync();
+        Task<WeeklySummaryDto> GetWeeklyOutboundSummaryAsync();
+        Task<TotalStockDto> GetTotalStockAsync();
+        Task<List<UnitOfMeasureDto>> GetUnitOfMeasuresAsync();
+        Task<PagedResult<InventoryListItemDto>> GetInventoryAsync(InventoryListQuery q);
+        Task<PagedResult<InventoryReportItemDto>> GetInventoryReportAsync(InventoryListQuery q);
+        Task<InventoryThreshold> UpdateMinStockAsync(int productId, int minStock);
+        Task<List<TraceSearchResultDto>> SearchTraceAsync(TraceSearchQuery query);
+        Task<ProductTraceDto?> GetProductTraceAsync(int productId);
+    }
+}
