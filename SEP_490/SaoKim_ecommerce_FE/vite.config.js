7fad6fc741d2dcce519a12824d5355ef0db6f891
Duong <trduong0224@gmail.com>
2025-12-18 22:23:15 +0700

    Update vite.config.js

--- SEP_490/SaoKim_ecommerce_FE/vite.config.js
+++ SEP_490/SaoKim_ecommerce_FE/vite.config.js
@@ -1,35 +1,13 @@
-import { cwd } from "node:process";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, cwd(), "");
-  const API_TARGET = env.VITE_API_TARGET || "https://datdovan.id.vn";
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: { "@": "/src" },
-    },
-    server: {
-      port: 5173,
-      strictPort: true,
-      proxy: {
-        "/api": {
-          target: API_TARGET,
-          changeOrigin: true,
-          secure: false,
-          ws: true,
-        },
-      },
-    },
-    preview: {
-      port: 4173,
-      strictPort: true,
-    },
-    build: {
-      outDir: "dist",
-      sourcemap: true,
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: { "@": "/src" },
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: true,
+  },
 });
